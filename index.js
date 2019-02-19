cd40bcbf02699b5bceeb002244cc76c60e372f4b
thanhhtth1807034 <thanhhtth1807034@fpt.edu.vn>
2019-02-19 22:15:31 +0700

    add order in admin and uploadfile cloudanary

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const fileUpload = require('express-fileupload');
+var cloudinary = require('cloudinary');
 const app = express();
 
-
-const mongoose = require("mongoose");
 // mongoose.connect('mongodb://localhost:27017/shoptify', {useNewUrlParser: true});
 mongoose.connect('mongodb://sa:abcd1234@ds163044.mlab.com:63044/shoptify', {useNewUrlParser: true});
 // mongoose.connect('mongodb://admin:admin123@ds331145.mlab.com:31145/hpny', {useNewUrlParser: true});
 
+cloudinary.config({
+    cloud_name: 'dhqw9z29d',
+    api_key: '172676653249292',
+    api_secret: '3AqERFmfqlB0_YotR51VVEuV06c'
+});
 
-// const fileUpload = require('express-fileupload');
-// var clouddinary = require('cloudinary');
+
+
 
 app.set("views", "views");
 app.set("views engine", "ejs");
-app.use(bodyParser.urlencoded());
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 // Tách phần xử lý chức năng routing ra một file bên ngoài.
 var router = require("./router/application-router.js");
